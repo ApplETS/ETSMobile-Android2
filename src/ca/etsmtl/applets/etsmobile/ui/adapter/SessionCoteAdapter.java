c2e3b6ad6918059a0710041e48586c12265396a9
ldev6 <laurence.de.villers.prog@gmail.com>
2014-02-05 21:23:05 -0500

    Bibliotheque page + début de note

---
+++ src/ca/etsmtl/applets/etsmobile/ui/adapter/SessionCoteAdapter.java
@@ -0,0 +1,58 @@
+package ca.etsmtl.applets.etsmobile.ui.adapter;
+
+
+import ca.etsmtl.applets.etsmobile2.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class SessionCoteAdapter extends BaseAdapter{
+	
+	private Context context;
+	private SessionCoteItem[] sessionCote;
+	public SessionCoteAdapter(Context context, SessionCoteItem[] sessionCoteItem) {
+		this.context = context;
+		this.sessionCote = sessionCoteItem;
+	}
+
+	@Override
+	public int getCount() {
+		if( sessionCote !=null){
+			return sessionCote.length;
+		}
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return sessionCote[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v;
+		if(convertView == null){
+		LayoutInflater inflater = (LayoutInflater) context
+		        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		    v = inflater.inflate(R.layout.square_note, parent, false);
+		}else{
+			v = convertView;
+		}
+		
+		  SessionCoteItem  notesSession = sessionCote[position];
+		   ((TextView) v.findViewById(R.id.square_note_course_name)).setText(notesSession.course);
+		   ((TextView)v.findViewById(R.id.square_note_course_cote)).setText(notesSession.cote);
+		   
+		return v;
+	}
+
+}
--- src/ca/etsmtl/applets/etsmobile/ui/fragment/NotesFragment.java
+++ src/ca/etsmtl/applets/etsmobile/ui/fragment/NotesFragment.java
@@ -2,6 +2,9 @@ package ca.etsmtl.applets.etsmobile.ui.fragment;
 
 
 import ca.etsmtl.applets.etsmobile.ui.adapter.NoteAdapter;
+import ca.etsmtl.applets.etsmobile.ui.adapter.NotesSessionItem;
+import ca.etsmtl.applets.etsmobile.ui.adapter.SessionCoteAdapter;
+import ca.etsmtl.applets.etsmobile.ui.adapter.SessionCoteItem;
 import ca.etsmtl.applets.etsmobile2.R;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -26,23 +29,32 @@ public class NotesFragment extends BaseFragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+	
+		View v = inflater.inflate(R.layout.activity_note, container,false);
+		NotesSessionItem[] notesSession = new NotesSessionItem[2];
+		SessionCoteItem[] sessionCoteItem = new SessionCoteItem[3];
+		sessionCoteItem[0] = new SessionCoteItem("GIA400", "A-");
+		sessionCoteItem[1] = new SessionCoteItem("LOG750","C");
+		sessionCoteItem[2] = new SessionCoteItem("GIA400", "A-");
+
 		
-		View v = inflater.inflate(R.layout.activity_note, container);
-		NotesSession[] notesSesssion = new NotesSession[4];
-		adapter = new NoteAdapter(getActivity(), R.layout.row_note_menu,notesSesssion);
+		SessionCoteItem[] sessionCoteItem2 = new SessionCoteItem[6];
+		sessionCoteItem2[0] = new SessionCoteItem("GIA400", "A-");
+		sessionCoteItem2[1] = new SessionCoteItem("LOG750","C");
+		sessionCoteItem2[2] = new SessionCoteItem("GIA400", "A-");
+		sessionCoteItem2[3] = new SessionCoteItem("LOG750","B+");
+		sessionCoteItem2[4] = new SessionCoteItem("GTI785","B+");
+		sessionCoteItem2[5] = new SessionCoteItem("GTI785","B+");
+		
+		SessionCoteAdapter sessionAdapter = new SessionCoteAdapter(getActivity(),sessionCoteItem);
+		SessionCoteAdapter sessionAdapter2 = new SessionCoteAdapter(getActivity(),sessionCoteItem2);
+		notesSession[0] = new NotesSessionItem("Hiver 2011",sessionAdapter);
+		notesSession[1] = new NotesSessionItem("Automne 2013",sessionAdapter2);
+		adapter = new NoteAdapter(getActivity(), R.layout.row_note_menu,notesSession);
 		mListView = (ListView) v.findViewById(R.id.activity_note_listview);
 		
 		mListView.setAdapter(adapter);
 		return v;	
 	}
 	
-	public class NotesSession{
-		public String sessionName;
-		public ArrayAdapter<SessionCote> arrayAdapter;
-	}
-	
-	public class SessionCote{
-		public String course;
-		public String cote;
-	}
 }
