e1773f7aa07e39248ceaa4c86cdb6171027d51d1
Philippe David <philippe.david0@gmail.com>
2014-03-06 16:22:25 -0500

    sponsors

--- src/ca/etsmtl/applets/etsmobile/ui/fragment/SponsorsFragment.java
+++ src/ca/etsmtl/applets/etsmobile/ui/fragment/SponsorsFragment.java
@@ -1,8 +1,20 @@
 package ca.etsmtl.applets.etsmobile.ui.fragment;
 
+import ca.etsmtl.applets.etsmobile2.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Phil on 18/11/13.
  */
 public class SponsorsFragment extends WebFragment {
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_sponsors, container, false);
+	}
 }
