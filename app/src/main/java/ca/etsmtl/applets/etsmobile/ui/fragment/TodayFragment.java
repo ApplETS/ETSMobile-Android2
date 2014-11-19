8c30cfd24c784e143d237e01bf11675bc063e9fc
eric4848 <eric.kavalec@gmail.com>
2014-11-18 19:28:20 -0500

    Fix bug today's fragment

--- app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/TodayFragment.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/TodayFragment.java
@@ -77,7 +77,7 @@ public class TodayFragment extends HttpFragment implements Observer {
             Date currentDate = new Date();
             Date dateStart;
             Date dateEnd;
-            for (int i = listeDeSessions.liste.size() - 1; i > 0; i++) {
+            for (int i = listeDeSessions.liste.size() - 1; i > 0; i--) {
                 dateStart = Utility.getDateFromString(listeDeSessions.liste.get(i).dateDebut);
                 dateEnd = Utility.getDateFromString(listeDeSessions.liste.get(i).dateFin);
                 if (currentDate.getTime() >= dateStart.getTime() && currentDate.getTime() <= dateEnd.getTime()) {
