fe2e4de60642efa8b53b4e4f8067e666b0388398
gnut3ll4 <thibaut.tauveron@gmail.com>
2014-12-14 13:57:51 -0500

    basic row new + display from earliest to latest

--- app/src/main/java/ca/etsmtl/applets/etsmobile/ui/adapter/NewsAdapter.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/adapter/NewsAdapter.java
@@ -6,9 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.octo.android.robospice.request.listener.RequestListener;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 
@@ -40,10 +45,20 @@ public class NewsAdapter extends ArrayAdapter<Nouvelle> {
             view = inflater.inflate(R.layout.row_news, parent, false);
             holder = new ViewHolder();
             holder.tvTitre = (TextView) view.findViewById(R.id.tv_row_news_titre);
+            holder.tvDate = (TextView) view.findViewById(R.id.tv_row_news_date);
+            holder.imageSource = (ImageView) view.findViewById(R.id.iv_news_source);
             view.setTag(holder);
         }
 
         Nouvelle item = getItem(position);
+        holder.tvTitre.setText(item.getTitle());
+        String updatedTime = item.getUpdated_time();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime date = dateTimeFormatter.parseDateTime(updatedTime);
+        DateTimeFormatter dateToDisplay = DateTimeFormat.forPattern("dd MMM yyyy");
+
+        holder.tvDate.setText(dateToDisplay.print(date));
         holder.tvTitre.setText(item.getTitle());
 
         return view;
@@ -51,6 +66,8 @@ public class NewsAdapter extends ArrayAdapter<Nouvelle> {
 
     static class ViewHolder {
         TextView tvTitre;
+        TextView tvDate;
+        ImageView imageSource;
 
     }
 }
--- app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/NewsFragment.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/NewsFragment.java
@@ -9,8 +9,13 @@ import android.widget.ListView;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
 import ca.etsmtl.applets.etsmobile.http.AppletsApiNewsRequest;
@@ -63,6 +68,29 @@ public class NewsFragment extends HttpFragment {
 
         if(o instanceof Nouvelles) {
             Nouvelles nouvelles = (Nouvelles)o;
+
+            Collections.sort(nouvelles, new Comparator<Nouvelle>() {
+                DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+                @Override
+                public int compare(Nouvelle nouvelle1, Nouvelle nouvelle2) {
+
+                    DateTime date1 = dateTimeFormatter.parseDateTime(nouvelle1.getUpdated_time());
+                    DateTime date2 = dateTimeFormatter.parseDateTime(nouvelle2.getUpdated_time());
+
+
+                    if(date1.isAfter(date2)){
+                        return -1;
+                    } else if (date1.isBefore(date2)) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+
+
+
             adapter = new NewsAdapter(getActivity(),R.layout.row_news, nouvelles,this);
             newsListView.setAdapter(adapter);
             adapter.notifyDataSetChanged();
