709acc09688d7b7b4615299dedbb3eabe0407ec2
gnut3ll4 <thibaut.tauveron@gmail.com>
2014-12-18 01:45:21 -0500

    Icons for news added

--- app/src/main/java/ca/etsmtl/applets/etsmobile/http/AppletsApiNewsRequest.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/http/AppletsApiNewsRequest.java
@@ -74,136 +74,136 @@ public class AppletsApiNewsRequest extends SpringAndroidSpiceRequest<Nouvelles> 
     private int assignResource(String key) {
         switch (key) {
             case "ets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_ets;
 
             case "substance":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_substance;
 
             case "centresportif":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_centresportif;
 
             case "applets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_applets;
 
             case "esports":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_esports;
 
             case "rafale":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_rafale;
 
             case "rockanddance":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_rockanddance;
 
             case "conjure":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_conjure;
 
             case "rockets":
-                return R.drawable.ic_courriel;
+                return R.drawable.ic_rockets;
 
             case "phoenix":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_phoenix;
 
             case "avioncargo":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_ace;
 
             case "clubcycliste":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_clubcycliste;
 
             case "football":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_football;
 
             case "ingenieuses":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_ingenieuses;
 
             case "debatpiranha":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_debatpiranha;
 
             case "radiopiranha":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_radiopiranha;
 
             case "walkingmachine":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_walkingmachine;
 
             case "atlhetsiques":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_athletsiques;
 
             case "aeets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_aeets;
 
             case "rugby":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_rugby;
 
             case "bibliotheque":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_bibliotheque;
 
             case "capra":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_capra;
 
             case "ieee":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_ieee;
 
             case "pontpop":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_pontpop;
 
             case "omer":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_omer;
 
             case "baja":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_baja;
 
             case "canoedebeton":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_canoedebeton;
 
             case "chinook":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_chinook;
 
             case "sonia":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_sonia;
 
             case "lanets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_lanets;
 
             case "formuleets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_formuleets;
 
             case "eclipse":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_eclipse;
 
             case "turbulence":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_turbulence;
 
             case "preci":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_preci;
 
             case "reflets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_reflets;
 
             case "crabeets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_crabeets;
 
             case "decliq":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_decliq;
 
             case "quiets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_quiets;
 
             case "dronolab":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_dronolab;
 
             case "liets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_liets;
 
             case "radiosansgenie":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_radiosansgenie;
 
             case "coopets":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_coopets;
 
             case "integrale":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_integrale;
 
             case "geniale":
-                return R.drawable.ic_launcher;
+                return R.drawable.ic_geniale;
             default:
                 return 0;
 
