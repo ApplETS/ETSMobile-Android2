3e4a8087d65f6db9896bd77e9cbd6eaf2ccce39f
gnut3ll4 <thibaut.tauveron@gmail.com>
2015-10-23 00:52:32 -0400

    Icons notifications

--- app/src/main/java/ca/etsmtl/applets/etsmobile/service/ETSGcmListenerService.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/service/ETSGcmListenerService.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -82,19 +84,21 @@ public class ETSGcmListenerService extends GcmService {
 //        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
 //                PendingIntent.FLAG_ONE_SHOT);
 
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_ets);
+
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_ets)
+                .setSmallIcon(R.drawable.school_48)
+                .setLargeIcon(bm)
                 .setContentTitle(notificationApplicationNom)
                 .setContentText(notificationTexte)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0, notificationBuilder.build());
     }
 
 }
