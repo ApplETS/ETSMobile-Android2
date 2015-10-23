c3833bb3f9fccf789273a69ac77d8d67803556f1
SLAF <simon.lafontaine.1@etsmtl.net>
2015-10-22 23:47:44 -0400

    Fixed the error displayed in the FAQ page when there's no internet connection

--- app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/FAQFragment.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/fragment/FAQFragment.java
@@ -1,11 +1,19 @@
 package ca.etsmtl.applets.etsmobile.ui.fragment;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import ca.etsmtl.applets.etsmobile.util.Utility;
 import ca.etsmtl.applets.etsmobile2.R;
 
 public class FAQFragment extends WebFragment {
@@ -21,7 +29,18 @@ public class FAQFragment extends WebFragment {
         View v = inflater.inflate(R.layout.fragment_faq, container, false);
         WebView faqWebView = (WebView) v.findViewById(R.id.faq_webview);
         faqWebView.getSettings().setJavaScriptEnabled(true);
-        faqWebView.loadUrl("http://www.clubapplets.ca/faq/");
+        faqWebView.getSettings().setAppCachePath(getActivity().getCacheDir().getAbsolutePath());
+        faqWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        if(Utility.isNetworkAvailable(getActivity())){
+            //faqWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+            faqWebView.loadUrl("http://www.clubapplets.ca/faq/");
+        } else {
+            //faqWebView.getSettings().get
+            Toast.makeText(getActivity(), "Une connexion internet est requise pour télécharger la page FAQ", Toast.LENGTH_LONG).show();
+        }
+
         return v;
     }
+
 }
