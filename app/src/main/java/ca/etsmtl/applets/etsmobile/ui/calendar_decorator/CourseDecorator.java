18b26d6efce2090d9366b6818bab8203a5fe1adf
komlan55 <sylvain.degue@gmail.com>
2016-03-30 21:32:31 -0400

    Ajout de Decorateur pour le Calendrier

--- app/src/main/java/ca/etsmtl/applets/etsmobile/ui/calendar_decorator/CourseDecorator.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/calendar_decorator/CourseDecorator.java
@@ -1,39 +1,51 @@
-package ca.etsmtl.applets.etsmobile.util;
+package ca.etsmtl.applets.etsmobile.ui.calendar_decorator;
 
+import android.content.Context;
 import android.graphics.Typeface;
-import android.text.style.BackgroundColorSpan;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.text.style.StyleSpan;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
+
+import ca.etsmtl.applets.etsmobile2.R;
 
 /**
  * Created by komlan on 28/03/16.
  */
 public class CourseDecorator implements DayViewDecorator {
 
-    private int color;
     private HashSet<CalendarDay> dates;
+    private Context context;
 
-    public CourseDecorator(int color, Collection<CalendarDay> dates) {
-        this.color = color;
+
+    public CourseDecorator(Context context, Collection<CalendarDay> dates) {
+
         this.dates = new HashSet<>(dates);
+        this.context = context;
+
     }
 
     @Override
     public boolean shouldDecorate(CalendarDay day) {
-        return dates.contains(day);
+        return dates.contains(day) && !day.getDate().equals(new Date());
     }
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.addSpan(new DotSpan(10, color));
+
+        view.addSpan(new DotSpan(15, ContextCompat.getColor(context, R.color.blue_ets)));
         view.addSpan(new StyleSpan(Typeface.BOLD));
+
 
 
     }
---
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/ui/calendar_decorator/FinalExamDecorator.java
@@ -0,0 +1,48 @@
+package ca.etsmtl.applets.etsmobile.ui.calendar_decorator;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.text.style.StyleSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+
+import ca.etsmtl.applets.etsmobile2.R;
+
+/**
+ * Created by komlan on 28/03/16.
+ */
+public class FinalExamDecorator implements DayViewDecorator {
+
+    private HashSet<CalendarDay> dates;
+    private Context context;
+
+
+    public FinalExamDecorator(Context context, Collection<CalendarDay> dates) {
+
+        this.dates = new HashSet<>(dates);
+        this.context = context;
+
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day) && !day.getDate().equals(new Date());
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+
+        view.addSpan(new DotSpan(15, ContextCompat.getColor(context, R.color.ets_red)));
+        view.addSpan(new StyleSpan(Typeface.BOLD));
+
+
+    }
+}
