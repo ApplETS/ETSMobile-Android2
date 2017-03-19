2709232213475c5e3c0aff686e27fb98e370ff7b
Sonphil <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2017-03-19 16:09:11 -0400

    Ajout de l'aperçu du widget
    Ajout d'un aperçu du widget dans l'activité de configuration

--- app/src/main/java/ca/etsmtl/applets/etsmobile/widget/TodayWidgetConfigureActivity.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/widget/TodayWidgetConfigureActivity.java
@@ -5,11 +5,27 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
+import android.support.v4.graphics.ColorUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.joda.time.DateTime;
+
+import java.util.Locale;
 
 import ca.etsmtl.applets.etsmobile.ui.adapter.ColorSpinnerAdapter;
 import ca.etsmtl.applets.etsmobile2.R;
@@ -19,16 +35,30 @@ import ca.etsmtl.applets.etsmobile2.R;
  */
 public class TodayWidgetConfigureActivity extends Activity {
 
+    //******************************
+    //CONSTANTES
+    //******************************
     private static final String PREFS_NAME = "ca.etsmtl.applets.etsmobile.TodayWidget";
     private static final String PREF_BG_COLOR_PREFIX_KEY = "bg_color_widget_";
     private static final String PREF_TEXT_COLOR_PREFIX_KEY = "text_color_widget_";
     private static final String PREF_OPACITY_PREFIX_KEY = "opacity_widget_";
 
-    int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-    SeekBar mOpacitySeekBar;
-    Spinner mBgColorSpinner;
-    Spinner mTextColorSpinner;
-    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+    //******************************
+    //ATTRIBUTS
+    //******************************
+    private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private SeekBar mOpacitySeekBar;
+    private ColorSpinnerAdapter mColorSpinnerAdapter;
+    private Spinner mBgColorSpinner;
+    private Spinner mTextColorSpinner;
+    private RelativeLayout mWidgetPreviewLayout;
+    private TextView mWidgetTodaysNameTv;
+    private ImageButton syncBtn;
+
+    //******************************
+    //ÉCOUTEURS
+    //******************************
+    View.OnClickListener mOkBtnOnClickListener = new View.OnClickListener() {
         public void onClick(View v) {
             final Context context = TodayWidgetConfigureActivity.this;
 
@@ -49,10 +79,93 @@ public class TodayWidgetConfigureActivity extends Activity {
         }
     };
 
+    SeekBar.OnSeekBarChangeListener mOpacityListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            int bgColor = Integer.parseInt(mBgColorSpinner.getSelectedItem().toString());
+            bgColor = ColorUtils.setAlphaComponent(bgColor, progress);
+
+            mWidgetPreviewLayout.setBackgroundColor(bgColor);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    Spinner.OnItemSelectedListener mBgColorSpinnerListener = new Spinner.OnItemSelectedListener() {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            int bgColor = Integer.parseInt(mBgColorSpinner.getSelectedItem().toString());
+            int opacity = mOpacitySeekBar.getProgress();
+            bgColor = ColorUtils.setAlphaComponent(bgColor, opacity);
+
+            mWidgetPreviewLayout.setBackgroundColor(bgColor);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+    Spinner.OnItemSelectedListener mTextColorSpinnerListener = new Spinner.OnItemSelectedListener() {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            int textColor = Integer.parseInt(mTextColorSpinner.getSelectedItem().toString());
+            DateTime dateActuelle = new DateTime();
+            DateTime.Property pDoW = dateActuelle.dayOfWeek();
+            DateTime.Property pDoM = dateActuelle.dayOfMonth();
+            DateTime.Property pMoY = dateActuelle.monthOfYear();
+            Locale locale = TodayWidgetConfigureActivity.this.getResources().getConfiguration()
+                    .locale;
+            String dateActuelleStr = TodayWidgetConfigureActivity.this.getString(R.string.horaire,
+                    pDoW.getAsText(locale),
+                    pDoM.getAsText(locale), pMoY.getAsText(locale));
+
+            mWidgetTodaysNameTv.setText(dateActuelleStr);
+            mWidgetTodaysNameTv.setTextColor(textColor);
+
+            Bitmap icon = BitmapFactory
+                    .decodeResource(TodayWidgetConfigureActivity.this.getResources(),
+                            R.drawable.ic_sync);
+
+            // Copie mutable de l'icône
+            icon = icon.copy(Bitmap.Config.ARGB_8888, true);
+            Paint paint = new Paint();
+            ColorFilter filter = new PorterDuffColorFilter(textColor, PorterDuff.Mode.SRC_IN);
+            paint.setColorFilter(filter);
+            Canvas canvas = new Canvas(icon);
+            canvas.drawBitmap(icon, 0, 0, paint);
+
+            syncBtn.setImageBitmap(icon);
+            syncBtn.setBackgroundColor(Color.TRANSPARENT);
+            syncBtn.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+    /**
+     * <h1>Constructeur par défaut</h1>
+     */
     public TodayWidgetConfigureActivity() {
         super();
     }
 
+    //******************************
+    //PROCÉDURES DE PRÉFÉRENCES
+    //******************************
     static void saveBgColorPref(Context context, int appWidgetId, int value) {
         SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
         prefs.putInt(PREF_BG_COLOR_PREFIX_KEY + appWidgetId, value);
@@ -106,24 +219,20 @@ public class TodayWidgetConfigureActivity extends Activity {
         setResult(RESULT_CANCELED);
 
         setContentView(R.layout.widget_today_configure);
-
-        mOpacitySeekBar = (SeekBar) findViewById(R.id.opacity_seekbar);
-
-        int[] colorsArray = getResources().getIntArray(R.array.widget_bg_colors);
-        Integer[] colors = new Integer[colorsArray.length];
-        for (int i = 0; i < colorsArray.length; i++) {
-            colors[i] = Integer.valueOf(colorsArray[i]);
-        }
-        ColorSpinnerAdapter colorSpinnerAdapter = new ColorSpinnerAdapter(this, colors);
+        mWidgetPreviewLayout = (RelativeLayout) findViewById(R.id.today_widget);
+        mWidgetTodaysNameTv = (TextView) findViewById(R.id.widget_todays_name);
+        syncBtn = (ImageButton) findViewById(R.id.widget_sync_btn);
 
         mBgColorSpinner = (Spinner) findViewById(R.id.bg_color_spinner);
-        mBgColorSpinner.setAdapter(colorSpinnerAdapter);
-        mBgColorSpinner.setSelection(0);
-        mTextColorSpinner = (Spinner) findViewById(R.id.bg_text_color_spinner);
-        mTextColorSpinner.setAdapter(colorSpinnerAdapter);
-        mTextColorSpinner.setSelection(0);
+        setUpColorSpinner(mBgColorSpinner, mBgColorSpinnerListener);
 
-        findViewById(R.id.ok_btn).setOnClickListener(mOnClickListener);
+        mTextColorSpinner = (Spinner) findViewById(R.id.bg_text_color_spinner);
+        setUpColorSpinner(mTextColorSpinner, mTextColorSpinnerListener);
+
+        mOpacitySeekBar = (SeekBar) findViewById(R.id.opacity_seekbar);
+        mOpacitySeekBar.setOnSeekBarChangeListener(mOpacityListener);
+
+        findViewById(R.id.ok_btn).setOnClickListener(mOkBtnOnClickListener);
 
         // Find the widget id from the intent.
         Intent intent = getIntent();
@@ -138,6 +247,21 @@ public class TodayWidgetConfigureActivity extends Activity {
             finish();
             return;
         }
+    }
+
+    private void setUpColorSpinner(Spinner spinner, AdapterView.OnItemSelectedListener onItemSelectedListener) {
+        if (mColorSpinnerAdapter == null) {
+            int[] colorsArray = getResources().getIntArray(R.array.widget_bg_colors);
+            Integer[] colors = new Integer[colorsArray.length];
+            for (int i = 0; i < colorsArray.length; i++) {
+                colors[i] = Integer.valueOf(colorsArray[i]);
+            }
+            mColorSpinnerAdapter = new ColorSpinnerAdapter(this, colors);
+        }
+
+        spinner.setAdapter(mColorSpinnerAdapter);
+        spinner.setOnItemSelectedListener(onItemSelectedListener);
+        spinner.setSelection(0);
     }
 }
 
